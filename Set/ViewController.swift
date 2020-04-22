3ba1dccc70b3131927f0fb1315e9107634df39e3
Vadym Bogdan <vadymbog@gmail.com>
2020-04-22 12:46:40 +0300

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Вадим on 22.04.2020.
+//  Copyright © 2020 Vadym Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
